7e20e75784492543d5683ab50476a1a89586d342
Andrii Muzalevskyi <amuzalevsky@magecore.com>
2015-04-23 13:55:44 +0300

    Update tests

--- test/jet_serializer.js
+++ test/jet_serializer.js
@@ -1,4 +1,5 @@
 var assert = require("assert"),
+    _ = require("underscore"),
     Jet_Serializer = require('../bin/jet_serializer');
 
 describe('Jet_Serializer', function(){
@@ -75,9 +76,7 @@ describe('Jet_Serializer', function(){
         var x = new d(),
             xRestored = Jet_Serializer.parse(Jet_Serializer.stringify(x));
         assert(xRestored instanceof d);
-        it('should restore class properties', function() {
-            assert.equal(xRestored.x, x.x);
-        });
+        assert.equal(xRestored.x, x.x);
     });
 
     it('should call $wakeup for objects', function() {
@@ -88,5 +87,106 @@ describe('Jet_Serializer', function(){
             xRestored = Jet_Serializer.parse(Jet_Serializer.stringify(x));
         assert(xRestored instanceof dw);
         assert(xRestored.wakedUp, true);
+    });
+
+
+    it('should not change initial object', function() {
+        function d () {
+            this.x = 5;
+        }
+
+        Jet_Serializer.registerClass('d', d);
+
+        var x = new d(),
+            y = new d(),
+            z = new d();
+
+        x.x = x;
+        x.y = y;
+        x.z = z;
+        x.name = "x";
+
+        y.x = x;
+        y.y = y;
+        y.z = z;
+        y.name = "y";
+
+        z.x = x;
+        z.y = y;
+        z.z = z;
+        z.name = "z";
+
+        Jet_Serializer.stringify(x);
+
+        assert.equal(x.x, x);
+        assert.equal(x.y, y);
+        assert.equal(x.z, z);
+
+        assert.equal(x.x.x, x);
+        assert.equal(x.x.y, y);
+        assert.equal(x.x.z, z);
+
+        assert.equal(x.y.x, x);
+        assert.equal(x.y.y, y);
+        assert.equal(x.y.z, z);
+
+        assert.equal(x.z.x, x);
+        assert.equal(x.z.y, y);
+        assert.equal(x.z.z, z);
+
+        assert.equal(x.x.name, 'x');
+        assert.equal(x.y.name, 'y');
+        assert.equal(x.z.name, 'z');
+
+        assert.equal(x.x.x.name, 'x');
+        assert.equal(x.x.y.name, 'y');
+        assert.equal(x.x.z.name, 'z');
+
+        assert.equal(x.y.x.name, 'x');
+        assert.equal(x.y.y.name, 'y');
+        assert.equal(x.y.z.name, 'z');
+
+        assert.equal(x.z.x.name, 'x');
+        assert.equal(x.z.y.name, 'y');
+        assert.equal(x.z.z.name, 'z');
+    });
+
+    it('should restore duplicate classes', function() {
+        function d () {
+            this.x = 5;
+        }
+
+        Jet_Serializer.registerClass('d', d);
+
+        var x = new d(),
+            y = new d(),
+            z = new d();
+
+        x.x = x;
+        x.y = y;
+        x.z = z;
+
+        y.x = x;
+        y.y = y;
+        y.z = z;
+
+        z.x = x;
+        z.y = y;
+        z.z = z;
+
+        var xRestored = Jet_Serializer.parse(Jet_Serializer.stringify(x));
+        assert(xRestored instanceof d);
+
+        assert.equal(xRestored.x, xRestored.x.x);
+        assert.equal(xRestored.x, xRestored.y.x);
+        assert.equal(xRestored.x, xRestored.z.x);
+
+        assert.equal(xRestored.y, xRestored.x.y);
+        assert.equal(xRestored.y, xRestored.y.y);
+        assert.equal(xRestored.y, xRestored.z.y);
+
+        assert.equal(xRestored.z, xRestored.x.z);
+        assert.equal(xRestored.z, xRestored.y.z);
+        assert.equal(xRestored.z, xRestored.z.z);
     });
 });
